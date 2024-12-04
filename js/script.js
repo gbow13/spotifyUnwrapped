5bcfb4803611fa72a454e43ff6551cb29e327188
gbow13 <mrgraemebowman@gmail.com>
2024-12-04 12:20:28 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -26,8 +26,8 @@ function calculateAndGenerate() {
   const resultMessage = `
     <p>Total Spotify subscription for the year: <strong><span class="highlight">£${yearlySubscription.toFixed(2)}</span></strong>.</p>
     <p>Spotify paid approximately <strong><span class="highlight">£${totalEarnings.toFixed(2)}</span></strong> to your favorite artist.</p>
-    <p>Fully independent artists will receive all of that money. Artists signed to record labels could receive just 20%-50% of this amount.</p>
-    <p style="font-weight: normal;">If you can, support your favourite artists in other ways like buying merch, attending live shows, or subscribing to their OnlyFans.</p>
+    <p style="font-weight: normal;">Fully independent artists will receive approx. 90% of that money. Artists signed to record labels could receive anywhere from 5% to 50% depending on their record deal.</p>
+    <p>If you can, support your favourite artists in other ways like buying merch, attending live shows, or subscribing to their OnlyFans.</p>
   `;
   document.getElementById('result').innerHTML = resultMessage;
 
